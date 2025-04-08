792e5ea9a73fe4912e4cd852b6ebed54b1c7c7a8
I Am Emptiness <145884361+UnknownHat832553@users.noreply.github.com>
2025-04-09 00:37:55 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,15 +1,13 @@
+import os
 from flask import Flask
-from threading import Thread
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Bot is Running!"
 
-def run():
-  app.run(host='0.0.0.0',port=8080)
-
-def server_on():
-    t = Thread(target=run)
-    t.start()
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    
